57a8ef381bad77f37bca5ffd1fea3152f414b11a
Arthur Borisow <arthur@Arthurs-MacBook-Pro.local>
2015-03-21 18:12:05 +0200

    Setup project

---
+++ deferred/deferred.h
@@ -0,0 +1,19 @@
+//
+//  deferred.h
+//  deferred
+//
+//  Created by Arthur Borisow on 3/21/15.
+//  Copyright (c) 2015 Arthur Borisow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for deferred.
+FOUNDATION_EXPORT double deferredVersionNumber;
+
+//! Project version string for deferred.
+FOUNDATION_EXPORT const unsigned char deferredVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <deferred/PublicHeader.h>
+
+
